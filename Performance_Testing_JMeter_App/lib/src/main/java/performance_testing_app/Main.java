8fef1363e6080d1e591e6719ac2a88eaee0e930f
evansmbithi <evholmbithy@gmail.com>
2023-07-03 17:03:07 +0300

    Add files via upload

---
+++ Performance_Testing_JMeter_App/lib/src/main/java/performance_testing_app/Main.java
@@ -0,0 +1,14 @@
+package performance_testing_app;
+
+import performance_testing_app.WebTesting;
+
+@SuppressWarnings("unused")
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebTesting.initSystemProperties();
+
+	}
+
+}
